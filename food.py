adbed692509ea4e83a64b3543beda7e2a9c80c88
ghdiqo <ghdiqo1@gmail.com>
2021-03-30 12:48:12 +0900

    램덤 추가 그리고

--- food.py
+++ food.py
@@ -1,17 +1,20 @@
-# 로또를  산다
-# 번호 1, 2, 3 아무꺼나 정해서
-# 당첨 번호를 선택 해서 결과를 출력
-
+import random
 ### 아래 전체를 반복 ###
 for i in range(1,4):
-    print("로또를 사자")
-    print("번호는? ")
-    print("1.당첨 2.2등 3.꽝")
-    menu = input(i + ".입력: ")
+    print("밥무러가자")
+    print("메뉴는? ")
+    # 메뉴 변수 나열
+    menulist = '쫄면', '육계장', '비빔밥'
+    print("1.쫄면 2.육계장 3.비빔밥 4.랜덤" )
+    menu = input(str(i) + ".입력: ")
     if menu == '1':
-        print("당첨")
+        print("쫄면")
     if menu == '2':
-        print("2등")
+        print("육계장")
     if menu == '3':
-        print("꽝")
+        print("비빔밥")
+    if menu == '4':
+        print("내맘데로")
+        print(random.choice(menulist))
+    # 랜덤을 고르면 위 메뉴중에서 아무꺼나
 ### 여기까지 반복 ###
